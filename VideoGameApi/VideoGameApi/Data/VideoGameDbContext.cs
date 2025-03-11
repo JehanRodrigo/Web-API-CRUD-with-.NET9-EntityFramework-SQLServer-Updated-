f0c0f2371457fc73a21c2ff38ce6fc513bc797f9
JehanRodrigo <jehanrodrigo31@gmail.com>
2025-03-11 20:26:48 +0530

    Package update detected !! and some un detected Changes

--- VideoGameApi/VideoGameApi/Data/VideoGameDbContext.cs
+++ VideoGameApi/VideoGameApi/Data/VideoGameDbContext.cs
@@ -4,6 +4,7 @@ namespace VideoGameApi.Data
 {
     public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
     {
+        public DbSet<VideoGame> VideoGames => Set<VideoGame>(); //creates a table called VideoGames and maps it to the VideoGame class
     }
     
 }
