2bf542ea74c1f0e74b33684a541c26968af824c3
JehanRodrigo <jehanrodrigo31@gmail.com>
2025-03-18 21:08:52 +0530

    Comments Added to Migration file

--- VideoGameApi/VideoGameApi/Migrations/20250318151008_Initial.cs
+++ VideoGameApi/VideoGameApi/Migrations/20250318151008_Initial.cs
@@ -12,26 +12,26 @@ namespace VideoGameApi.Migrations
         {
             migrationBuilder.CreateTable(
                 name: "VideoGames",
-                columns: table => new
+                columns: table => new //creates a table called VideoGames with the following columns
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Platform = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Developer = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                    Id = table.Column<int>(type: "int", nullable: false) //creates a column called Id with the type int
+                        .Annotation("SqlServer:Identity", "1, 1"), //sets the column to auto increment
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true), //creates a column called Title with the type string
+                    Platform = table.Column<string>(type: "nvarchar(max)", nullable: true), //creates a column called Platform with the type string
+                    Developer = table.Column<string>(type: "nvarchar(max)", nullable: true), //creates a column called Developer with the type string
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: true) //creates a column called Publisher with the type string
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_VideoGames", x => x.Id);
+                    table.PrimaryKey("PK_VideoGames", x => x.Id); //sets the Id column as the primary key
                 });
         }
 
         /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
+        protected override void Down(MigrationBuilder migrationBuilder) //deletes the table VideoGames if it exists 
         {
-            migrationBuilder.DropTable(
-                name: "VideoGames");
+            migrationBuilder.DropTable( 
+                name: "VideoGames"); 
         }
     }
 }
