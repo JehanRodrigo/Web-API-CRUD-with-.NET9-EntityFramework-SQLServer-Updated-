ce5ea83e5e86ae1c2d02fe24c52859d47546ce20
JehanRodrigo <jehanrodrigo31@gmail.com>
2025-05-12 12:59:30 +0530

    First /api/VideoGame Endpoint created

--- VideoGameApi/VideoGameApi/Controllers/VideoGameController.cs
+++ VideoGameApi/VideoGameApi/Controllers/VideoGameController.cs
@@ -1,67 +1,72 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Data;
 
 namespace VideoGameApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class VideoGameController : ControllerBase
+    public class VideoGameController(VideoGameDbContext context) : ControllerBase 
     {
-        
-        [HttpGet]
-        public ActionResult<List<VideoGame>> GetVideoGames() //gets list of video games
-        {
-            return Ok(videoGames);
-        }
+        private readonly VideoGameDbContext _context = context; //this is the database context
+
+       
 
         [HttpGet]
-        [Route("{id}")]
-        //or [Http("{id}")]
-        public ActionResult<VideoGame> GetVideoGameById(int id) //this time we are getting single video game
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGames() //gets list of video games
         {
-            var game = videoGames.FirstOrDefault(g => g.Id == id); //*lambda expression* gets an object as g where Id is equal to the provided id 
-            if (game == null) //if there's no matching Id
-                return NotFound(); // returns status code 404 not found
-
-            return Ok(game); // return the game with 200 OK
+            return Ok(await _context.VideoGames.ToListAsync());
         }
 
-        [HttpPost]
+        //[HttpGet]
+        //[Route("{id}")]
+        ////or [Http("{id}")]
+        //public ActionResult<VideoGame> GetVideoGameById(int id) //this time we are getting single video game
+        //{
+        //    var game = videoGames.FirstOrDefault(g => g.Id == id); //*lambda expression* gets an object as g where Id is equal to the provided id 
+        //    if (game == null) //if there's no matching Id
+        //        return NotFound(); // returns status code 404 not found
 
-        public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
-        {
-            if (newGame is null)
-                return BadRequest(); //returns status code 400 bad request
+        //    return Ok(game); // return the game with 200 OK
+        //}
 
-            newGame.Id = videoGames.Max(g => g.Id) + 1; //finds the max Id and adds 1 to it
-            videoGames.Add(newGame); //adds the new game to the list
-            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame); //returns status code 201 created
-        }
+        //[HttpPost]
 
-        [HttpPut("{id}")]
+        //public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
+        //{
+        //    if (newGame is null)
+        //        return BadRequest(); //returns status code 400 bad request
 
-        public ActionResult<VideoGame> UpdateVideoGame(int id, VideoGame updatedGame)
-        {
-            var game = videoGames.FirstOrDefault(g => g.Id == id); //finds the game with the provided Id
-            if (game == null)
-                return NotFound(); //returns status code 404 not found
+        //    newGame.Id = videoGames.Max(g => g.Id) + 1; //finds the max Id and adds 1 to it
+        //    videoGames.Add(newGame); //adds the new game to the list
+        //    return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame); //returns status code 201 created
+        //}
 
-            game.Title = updatedGame.Title; //updates the title
-            game.Platform = updatedGame.Platform; //updates the platform
-            game.Developer = updatedGame.Developer; //updates the developer
-            game.Publisher = updatedGame.Publisher; //updates the publisher
-            return Ok(game); //returns status code 200 OK
-        }
+        //[HttpPut("{id}")]
 
-        [HttpDelete("{id}")]
+        //public ActionResult<VideoGame> UpdateVideoGame(int id, VideoGame updatedGame)
+        //{
+        //    var game = videoGames.FirstOrDefault(g => g.Id == id); //finds the game with the provided Id
+        //    if (game == null)
+        //        return NotFound(); //returns status code 404 not found
 
-        public ActionResult DeleteVideoGame(int id) {
-            var game = videoGames.FirstOrDefault(g => g.Id == id); //finds the game with the provided Id
-            if (game == null)
-                return NotFound(); //returns status code 404 not found
-            videoGames.Remove(game); //removes the game from the list
-            return NoContent(); //returns status code 204 no content
-        }
+        //    game.Title = updatedGame.Title; //updates the title
+        //    game.Platform = updatedGame.Platform; //updates the platform
+        //    game.Developer = updatedGame.Developer; //updates the developer
+        //    game.Publisher = updatedGame.Publisher; //updates the publisher
+        //    return Ok(game); //returns status code 200 OK
+        //}
+
+        //[HttpDelete("{id}")]
+
+        //public ActionResult DeleteVideoGame(int id) {
+        //    var game = videoGames.FirstOrDefault(g => g.Id == id); //finds the game with the provided Id
+        //    if (game == null)
+        //        return NotFound(); //returns status code 404 not found
+        //    videoGames.Remove(game); //removes the game from the list
+        //    return NoContent(); //returns status code 204 no content
+        //}
     }
 }
